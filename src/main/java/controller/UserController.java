68e2d3706e867ac490dd5f9ab3ec2b7d773468bb
wangyang <541230721@qq.com>
2016-06-28 19:33:43 +0800

    robot

--- src/main/java/controller/UserController.java
+++ src/main/java/controller/UserController.java
@@ -1,6 +1,9 @@
 package controller;
 
 import enums.UserEnum;
+import framwork.message.Attachment;
+import framwork.message.MailSender;
+import framwork.message.MailSenderConfig;
 import model.User;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
@@ -13,6 +16,7 @@ import service.UserService;
 import utils.RegexUtil;
 import vo.BaseResult;
 
+import java.io.File;
 import java.util.Date;
 
 /**
@@ -84,7 +88,7 @@ public class UserController {
 
     @RequestMapping(value = "/addUser", method = RequestMethod.POST)
     public String addUser(@PathVariable("username") String username, @PathVariable("password") String password,
-                         @PathVariable("email") String email, ModelMap map) {
+                         @PathVariable("email") String email, ModelMap map) throws Exception {
 
         if (username == null || password == null || email == null) {
             return "redirect:/user/toRegist";
@@ -105,7 +109,22 @@ public class UserController {
             return "redirect:/user/toRegist";
         }
         //TODO 发送邮件验证
+        String SMTP_MAIL_HOST = "smtp.163.com";// 此邮件服务器地址
+        String EMAIL_USERNAME = "11133115wyd@163.com";
+        String EMAIL_PASSWORD = "11133115";
+        String TO_EMAIL_ADDRESS_1 = email;
 
+        MailSenderConfig c = new MailSenderConfig(SMTP_MAIL_HOST
+                , "激活账户邮件"
+                , "<h3>请点击下面的链接激活你的账户</h3><dr/><a href=''></a>"
+                , EMAIL_USERNAME);
+        c.setUsername(EMAIL_USERNAME);
+        c.setPassword(EMAIL_PASSWORD);
+        c.addToMail(TO_EMAIL_ADDRESS_1);
+        c.addCcMail(TO_EMAIL_ADDRESS_1);
+        c.addAttachment(new Attachment(new File("d:/1.txt")));
+        MailSender ms = new MailSender(c);
+        ms.send();
 
         return "/sendemail";
     }
